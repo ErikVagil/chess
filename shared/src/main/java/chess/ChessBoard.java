83f2dc8375f8e00167f551d93284ece1c0c6c457
Erik Vagil <erikvagil2734@gmail.com>
2024-01-17 10:24:32 -0700

    Fixed getters, setters, and resetBoard() in ChessBoard, overrided toString()

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.lang.StringBuilder;
+
+import chess.ChessGame.TeamColor;
+import chess.ChessPiece.PieceType;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -20,8 +25,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        int x = position.getRow();
-        int y = position.getColumn();
+        int x = position.getRow() - 1;
+        int y = position.getColumn() - 1;
         this.layout[x][y] = piece;
     }
 
@@ -33,8 +38,8 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        int x = position.getRow();
-        int y = position.getColumn();
+        int x = position.getRow() - 1;
+        int y = position.getColumn() - 1;
         ChessPiece pieceAtPosition = this.layout[x][y];
         return pieceAtPosition;
     }
@@ -44,6 +49,79 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        this.layout = new ChessPiece[8][8];
+        ChessPiece[][] newBoard = new ChessPiece[8][8];
+        TeamColor color = TeamColor.BLACK;
+        // Row 8 - r n b q k b n r
+        newBoard[7][0] = new ChessPiece(color, PieceType.ROOK);
+        newBoard[7][1] = new ChessPiece(color, PieceType.KNIGHT);
+        newBoard[7][2] = new ChessPiece(color, PieceType.BISHOP);
+        newBoard[7][3] = new ChessPiece(color, PieceType.QUEEN);
+        newBoard[7][4] = new ChessPiece(color, PieceType.KING);
+        newBoard[7][5] = new ChessPiece(color, PieceType.BISHOP);
+        newBoard[7][6] = new ChessPiece(color, PieceType.KNIGHT);
+        newBoard[7][7] = new ChessPiece(color, PieceType.ROOK);
+
+        // Row 7 - p p p p p p p p
+        for (int i = 0; i < 8; i++) {
+            newBoard[6][i] = new ChessPiece(color, PieceType.PAWN);
+        }
+
+        // Row 2 - P P P P P P P P
+        color = TeamColor.WHITE;
+        for (int i = 0; i < 8; i++) {
+            newBoard[1][i] = new ChessPiece(color, PieceType.PAWN);
+        }
+
+        // Row 1 - R N B Q K B N R
+        newBoard[0][0] = new ChessPiece(color, PieceType.ROOK);
+        newBoard[0][1] = new ChessPiece(color, PieceType.KNIGHT);
+        newBoard[0][2] = new ChessPiece(color, PieceType.BISHOP);
+        newBoard[0][3] = new ChessPiece(color, PieceType.QUEEN);
+        newBoard[0][4] = new ChessPiece(color, PieceType.KING);
+        newBoard[0][5] = new ChessPiece(color, PieceType.BISHOP);
+        newBoard[0][6] = new ChessPiece(color, PieceType.KNIGHT);
+        newBoard[0][7] = new ChessPiece(color, PieceType.ROOK);
+
+        // Set instance variable
+        this.layout = newBoard;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 0; j < 8; j++) {
+                output.append("|");
+                ChessPiece currentPiece = this.layout[i][j];
+                if (currentPiece == null) {
+                    output.append(" ");
+                    continue;
+                }
+                switch (currentPiece.getPieceType()) {
+                    case PieceType.KING:
+                        output.append("k");
+                        break;
+                    case PieceType.QUEEN:
+                        output.append("q");
+                        break;
+                    case PieceType.BISHOP:
+                        output.append("b");
+                        break;
+                    case PieceType.KNIGHT:
+                        output.append("n");
+                        break;
+                    case PieceType.ROOK:
+                        output.append("r");
+                        break;
+                    case PieceType.PAWN:
+                        output.append("p");
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid piece");
+                }
+            }
+            output.append("|\n");
+        }
+        return output.toString();
     }
 }
