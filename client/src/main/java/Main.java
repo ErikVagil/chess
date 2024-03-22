1f2d3044331a636d01fe84a0d255411487c60410
Erik Vagil <erikvagil2734@gmail.com>
2024-03-22 15:21:46 -0600

    Implemented login and register commands in prelogin

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,7 +1,10 @@
+import server.Server;
 import ui.ServerFacade;
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
         ServerFacade client = new ServerFacade();
         client.run();
     }
--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,13 +1,27 @@
 package ui;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+import model.*;
 public class ServerFacade {
     private boolean loggedIn;
+    private String sessionToken;
+    private String displayName;
     
     public ServerFacade() {
-        this.loggedIn = false;
+        loggedIn = false;
+        sessionToken = null;
+        displayName = null;
     }
 
     public void run() {
@@ -20,6 +34,8 @@ public class ServerFacade {
         while (!loggedIn && running) {
             System.out.print("[" + EscapeSequences.SET_TEXT_COLOR_RED + "LOGGED OUT" + EscapeSequences.RESET_TEXT_COLOR + "] >>> ");
             String input = scanner.nextLine();
+
+            // Parse input into tokens to get params
             List<String> inputTokens;
             inputTokens = Arrays.asList(input.split(" "));
 
@@ -53,10 +69,55 @@ public class ServerFacade {
                                        "Exiting program...");
                     break;
                 case "login":
-                    System.out.println(inputTokens.toString());
+                    // Check command args
+                    if (inputTokens.size() != 3) {
+                        System.out.println("Proper usage of this command is:" +
+                                           EscapeSequences.SET_TEXT_COLOR_BLUE +
+                                           " login" +
+                                           EscapeSequences.SET_TEXT_COLOR_MAGENTA +
+                                           " <USERNAME> <PASSWORD>");
+                        System.out.println(EscapeSequences.RESET_TEXT_COLOR +
+                                           "Type \"help\" for more information.");
+                        break;
+                    }
+                    
+                    // Call login on server
+                    String username = inputTokens.get(1);
+                    String password = inputTokens.get(2);
+                    try {
+                        AuthData auth = clientLogin(username, password);
+                        sessionToken = auth.authToken;
+                        displayName = auth.username;
+                        System.out.println("Successfully logged in!");
+                    } catch (Exception e) {
+                        System.out.println("Could not log in. Please try again.");
+                    }
                     break;
                 case "register":
-                    System.out.println(inputTokens.toString());
+                    // Check command args
+                    if (inputTokens.size() != 4) {
+                        System.out.println("Proper usage of this command is:" +
+                                           EscapeSequences.SET_TEXT_COLOR_BLUE +
+                                           " register" +
+                                           EscapeSequences.SET_TEXT_COLOR_MAGENTA +
+                                           " <USERNAME> <PASSWORD> <EMAIL>");
+                        System.out.println(EscapeSequences.RESET_TEXT_COLOR +
+                                           "Type \"help\" for more information.");
+                        break;
+                    }
+                    
+                    // Call register on server
+                    username = inputTokens.get(1);
+                    password = inputTokens.get(2);
+                    String email = inputTokens.get(3);
+                    try {
+                        AuthData auth = clientRegister(username, password, email);
+                        sessionToken = auth.authToken;
+                        displayName = auth.username;
+                        System.out.println("Successfully registered!");
+                    } catch (Exception e) {
+                        System.out.println("Could not register. Please try again.");
+                    }
                     break;
                 default:
                     System.out.println("Please enter a valid command. Type \"help\" for more information.");
@@ -65,5 +126,51 @@ public class ServerFacade {
         }
 
         scanner.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    private AuthData clientLogin(String username, String password) throws Exception {
+        URI uri = new URI("http://localhost:8080/session");
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod("POST");
+
+        http.setDoOutput(true);
+
+        http.addRequestProperty("Content-Type", "application/json");
+        Map<String, String> reqBody = Map.of("username", username, "password", password);
+        try (OutputStream outputStream = http.getOutputStream()) {
+            String jsonBody = new Gson().toJson(reqBody);
+            outputStream.write(jsonBody.getBytes());
+        }
+
+        http.connect();
+
+        try (InputStream inputStream = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            Map<String, String> res = new Gson().fromJson(inputStreamReader, Map.class);
+            return new AuthData(res.get("authToken"), res.get("username"));
+        }
+    }
+
+    private AuthData clientRegister(String username, String password, String email) throws Exception {
+        URI uri = new URI("http://localhost:8080/user");
+        HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+        http.setRequestMethod("POST");
+
+        http.setDoOutput(true);
+
+        http.addRequestProperty("Content-Type", "application/json");
+        UserData reqBody = new UserData(username, password, email);
+        try (OutputStream outputStream = http.getOutputStream()) {
+            String jsonBody = new Gson().toJson(reqBody, UserData.class);
+            outputStream.write(jsonBody.getBytes());
+        }
+
+        http.connect();
+
+        try (InputStream inputStream = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            return new Gson().fromJson(inputStreamReader, model.AuthData.class);
+        }
     }
 }
